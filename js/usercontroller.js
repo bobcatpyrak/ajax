1d7ae1ebec129cf28e2d159b2b0e8e99d876a4f0
Jonathan Pyrak <bobcatpyrak@gmail.com>
2020-11-06 12:24:58 -0500

    added getAll to usercontroller.js

---
+++ js/usercontroller.js
@@ -0,0 +1,40 @@
+$().ready(() =>
+{
+    var users;
+
+    const baseurl = "http://localhost:8080/api";
+
+    const getAll = () =>
+    {
+        $.getJSON(`${baseurl}/users/`)
+            .done(users => 
+            {
+                display(users);
+                console.log(users);
+            })       
+    }
+
+    const display = (users) =>
+    {
+        let tbody = $("tbody");
+        tbody.empty();
+        for(let u of users)
+        {
+                    
+            let tr = $("<tr></tr>");
+            let td = $(`<td>${u.id}</td>
+                        <td>${u.firstName}</td>
+                        <td>${u.lastName}</td>
+                         <td>${u.username}</td>
+                        <td>****</td>
+                        <td>${u.email}</td>
+                        <td>${u.phoneNumber}</td>
+                        <td>${u.admin}</td>
+                        <td>${u.reviewer}</td>`);
+            tr.append(td);
+            tbody.append(tr);
+        }  
+    }
+
+    getAll();
+});
